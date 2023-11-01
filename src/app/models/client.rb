fc9a6727259bc504619559b31aba600b56390221
Team Account <team@hpc308.tcnj.edu>
2023-11-01 14:13:00 -0400

    Add getter for age

--- src/app/models/client.rb
+++ src/app/models/client.rb
@@ -13,4 +13,18 @@ class Client < ApplicationRecord
     # def add_restriction(restriction)
     #   self.restrictions << restriction
     # end
+
+  # Custom method to derive the age of a client based on their date of birth.
+  def age
+    # What day is it today?
+    today = Date.today
+    # The client is at most as old as the difference between the years.
+    result = today.year - dob.year
+    # If their birthday hasn't yet passed this year, subtract one from the result.
+    if today.month < dob.month or (today.month == dob.month and today.day < dob.day)
+      result -= 1
+    end
+    # Return final result.
+    result
   end
+end
