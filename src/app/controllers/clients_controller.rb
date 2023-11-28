7338fc87b684d01f145f990dbb15d2d2f1d29a9a
Team Account <team@hpc308.tcnj.edu>
2023-11-28 13:13:06 -0500

    working edit final

--- src/app/controllers/clients_controller.rb
+++ src/app/controllers/clients_controller.rb
@@ -1,57 +1,61 @@
 class ClientsController < ApplicationController
-    def new
-      @client = Client.new
-    end
-
-    
-
-    def edit
-      @client = Client.find(params[:id])
-    end
-
-    def update
-      @client = Client.find(params[:id])
-      if @client.update(client_params)
-        flash[:notice] = "Client profile updated successfully!"
-        redirect_to client_path(@client)
-      else
-        flash.now[:alert] = "Failed to update client profile."
-        render :edit
-      end
-    end
-
-    
-    
-    
+  def new
+    @client = Client.new
+  end
 
   
-    def create
-      cparams = client_params
-      puts cparams
-      cparams[:dob] = Date.strptime(cparams[:dob], '%m/%d/%Y')
-      puts cparams
-      @client = Client.new(cparams)
-      if @client.save
-        current_user.client = @client
-        current_user.save
-        flash[:notice] = "Client profile created successfully!"
-        redirect_to client_path(@client)  # Redirect to the show action
-      else
-        flash.now[:alert] = "Failed to create client profile."
-        render :new
-      end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      flash[:notice] = "Client profile updated successfully!"
+      redirect_to client_path(@client)
+    else
+      flash.now[:alert] = "Failed to update client profile."
+      render :edit
     end
-    
-    def show
-      @client = Client.find(params[:id])
-      render :client
+  end
+
+  
+  
+  
+
+
+  def create
+    cparams = client_params
+    puts cparams
+    cparams[:dob] = Date.strptime(cparams[:dob], '%m/%d/%Y')
+    puts cparams
+    @client = Client.new(cparams)
+    if @client.save
+      current_user.client = @client
+      current_user.save
+      flash[:notice] = "Client profile created successfully!"
+      redirect_to client_path(@client)  # Redirect to the show action
+    else
+      flash.now[:alert] = "Failed to create client profile."
+      render :new
     end
-    
+  end
   
-    private
+  def show
+    @client = Client.find(params[:id])
+    render :client
+  end
   
-    def client_params
-      params.require(:client).permit(
+
+  private
+
+  
+  def client_params
+    params.require(:first_name)
+    params.require(:last_name)
+    params.require(:dob)
+    params.permit(
         :first_name,
         :last_name,
         :dob,
@@ -61,9 +65,9 @@ class ClientsController < ApplicationController
         :gluten_free,
         :vegan,
         :restrictions,
+        # Permits all common allergen parameters
         *Client::COMMON_ALLERGENS.map(&:first),
       )
-    end
-    
   end
   
+end
