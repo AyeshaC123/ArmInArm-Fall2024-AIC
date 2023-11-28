c49782d2a401a3cce4d695af8c8e7cd3abc43527
George Agyei-Sam <samg1@tcnj.edu>
2023-11-28 03:43:33 -0500

    test commit

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,6 +3,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+
+  #
+
   # Admin route
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
